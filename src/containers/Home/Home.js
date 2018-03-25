1954379f2136e7af2c89d15c03b205a30608e14e
Tiago Donatti <tdonatti777@gmail.com>
2018-03-25 15:03:06 -0400

    Home container, reducer and unit test

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -7,51 +7,40 @@ import { withRouter } from 'react-router-dom';
 import Helmet from 'react-helmet';
 import { hot } from 'react-hot-loader';
 
-import * as actionUsers from '../../actions/users';
-import type { Home as HomeType, Dispatch, ReduxState } from '../../types';
-import { UserList } from '../../components';
+import type { Home as HomeType, ReduxState } from '../../types';
+
 import styles from './styles.scss';
 
-type Props = { home: HomeType, fetchUsersIfNeeded: () => void };
+type Props = { home: HomeType };
 
 // Export this for unit testing more easily
 export class Home extends PureComponent<Props> {
-  componentDidMount() {
-    this.props.fetchUsersIfNeeded();
-  }
+  componentDidMount() {}
 
-  renderUserList = () => {
+  renderSearchForm = () => {
     const { home } = this.props;
-
-    if (
-      !home.readyStatus ||
-      home.readyStatus === 'USERS_INVALID' ||
-      home.readyStatus === 'USERS_REQUESTING'
-    ) {
-      return <p>Loading...</p>;
-    } else if (home.readyStatus === 'USERS_FAILURE') {
-      return <p>Oops, Failed to load list!</p>;
+    if (home) {
+      return (
+        <div>
+          <input type="text" />
+        </div>
+      );
     }
 
-    return <UserList list={home.list} />;
+    return <div />;
   };
 
   render() {
     return (
       <div className={styles.Home}>
         <Helmet title="Home" />
-        {this.renderUserList()}
+        {this.renderSearchForm()}
       </div>
     );
   }
 }
 
-const connector = connect(
-  ({ home }: ReduxState) => ({ home }),
-  (dispatch: Dispatch) => ({
-    fetchUsersIfNeeded: () => dispatch(actionUsers.fetchUsersIfNeeded())
-  })
-);
+const connector = connect(({ home }: ReduxState) => ({ home }));
 
 // Enable hot reloading for async componet
 export default compose(hot(module), withRouter, connector)(Home);
