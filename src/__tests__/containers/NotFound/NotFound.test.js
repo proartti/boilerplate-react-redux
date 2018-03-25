e30615c38ee00fff584886986ee61618da3fe564
Tiago Donatti <tdonatti777@gmail.com>
2018-03-25 15:04:26 -0400

    NotFound Component and unit test

---
+++ src/__tests__/containers/NotFound/NotFound.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { StaticRouter } from 'react-router-dom';
+
+import NotFound from '../index';
+
+describe('<NotFound />', () => {
+  test('renders', () => {
+    const tree = renderer
+      .create(
+        <StaticRouter context={{}}>
+          <NotFound />
+        </StaticRouter>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+});
